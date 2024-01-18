99bdc0e39666445abea6f5230a366a6d6651fd47
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2024-01-18 22:41:37 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -3,11 +3,10 @@
 echo "-----  Starting server...----- "
 Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9'}
 
-nohup ./server tunnel --edge-ip-version auto run --token $Token >/dev/null 2>&1 &
+nohup ./server tunnel --edge-ip-version auto run --token $Token > server.log 2>&1 &
 
 echo "-----  Starting web ...----- ."
 
-nohup ./web >/dev/null 2>&1 &
-
+nohup ./web > web.log 2>&1 &
 
 tail -f /dev/null
