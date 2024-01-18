cc44d461a9f660d225f936291b334eb4261b1cab
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2024-01-18 21:23:26 +0800

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "-----  Starting server...----- "
+Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9'}
+
+nohup ./server tunnel --edge-ip-version auto run --token $Token >/dev/null 2>&1 &
+
+echo "-----  Starting web ...----- ."
+
+nohup ./web >/dev/null 2>&1 &
+
+
+tail -f /dev/null
