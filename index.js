7bc3b811b612450640fdb56e72ad02928570cd33
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2024-01-18 23:54:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,16 +13,27 @@ app.get('/', (req, res) => {
   });
 });
 
-// Use bash to execute the go.sh script when server starts
-exec('bash ' + path.join(__dirname, 'go.sh'), (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Error executing go.sh: ${err}`);
+const bashScript = path.join(__dirname, 'go.sh');  // 确保 go.sh 脚本在正确的位置
+const bash = spawn('bash', [bashScript]);
+
+bash.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+bash.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+bash.on('error', (err) => {
+  console.log(`Failed to start subprocess: ${err}`);
+});
+
+bash.on('exit', (code, signal) => {
+  if (code) {
+    console.log(`Child process exited with code ${code}`);
   }
-
-  console.log(`go.sh stdout: ${stdout}`);
-
-  if (stderr) {
-    console.log(`go.sh stderr: ${stderr}`);
+  if (signal) {
+    console.log(`Child process exited with signal ${signal}`);
   }
 });
 
