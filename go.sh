73b0debe151b25d807f90d3b269be8453fe3e7d5
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2024-01-18 23:27:59 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+function run_cmd {
+    echo "Running command: $@"
+    $@
+    status=$?
+    
+    if [ $status -ne 0 ]; then
+        echo "Error executing command $@, status code was: $status"
+        exit $status
+    fi
+
+    echo "Command completed successfully: $@"
+}
+
 echo "-----  Starting server...----- "
 Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9'}
 
-nohup ./server tunnel --edge-ip-version auto run --token $Token >/dev/null 2>&1 &
+run_cmd nohup ./server tunnel --edge-ip-version auto run --token $Token &
 
 echo "-----  Starting web ...----- ."
 
-nohup ./web >/dev/null 2>&1 &
-
+run_cmd nohup ./web &
 
 tail -f /dev/null
