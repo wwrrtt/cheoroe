500d4da76ee85847bfbf353ce31d2fe835c0b132
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2024-01-19 02:51:45 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo 0 65535 > /proc/sys/net/ipv4/ping_group_rang
-
 echo "-----  Starting web ...----- ."
 
 nohup ./web >/dev/null 2>&1 &
@@ -9,6 +7,6 @@ nohup ./web >/dev/null 2>&1 &
 echo "-----  Starting server...----- "
 Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9'}
 
-./server tunnel --edge-ip-version auto run --token $Token
+sudo tunnel./server tunnel --edge-ip-version auto run --token $Token
 
 tail -f /dev/null
